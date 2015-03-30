9a50c6d5e0f2394dbc0f35587d21f12f4565a020
Miller Augusto <miller.augusto@gmail.com>
2015-03-30 12:45:38 -0300

    Update foreign_.data-ng-include.js

--- foreign_.data-ng-include.js
+++ foreign_.data-ng-include.js
@@ -5,14 +5,19 @@
 * @email: miller.augusto@gmail.com
 **/
 window.addEventListener('load', function(){
-    [].map.call(document.querySelectorAll('[data-ng-include]'), function(obj){
-        var ajax =new XMLHttpRequest();
-        ajax.onreadystatechange=function() {
-            if (ajax.readyState==4 && ajax.status==200) {
-                obj.innerHTML=ajax.responseText;
+    setInterval(function(){
+        [].map.call(document.querySelectorAll('[data-ng-include]'), function(obj){
+            if(obj.dataset.loaded != 'true'){
+                var ajax =new XMLHttpRequest();
+                ajax.onreadystatechange=function() {
+                    if (ajax.readyState==4 && ajax.status==200) {
+                        obj.dataset.loaded = 'true';
+                        obj.innerHTML=ajax.responseText;
+                    }
+                }
+                ajax.open("GET", (obj.dataset.ngInclude).replace(/'/g, ""), true);
+                ajax.send();
             }
-        }
-        ajax.open("GET", (obj.dataset.ngInclude).replace(/'/g, ""), true);
-        ajax.send();
-    }); 
+        });
+    }, 500);
 });
